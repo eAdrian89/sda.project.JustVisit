83feb4dcc5fb358e008737c780e6ac079f7fcdd1
Rafal <ravmaster48@gmail.com>
2021-05-08 12:37:18 +0100

    initial commit

---
+++ src/main/java/com/justvisit/justvisit/JustvisitApplication.java
@@ -0,0 +1,13 @@
+package com.justvisit.justvisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustvisitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustvisitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/justvisit/justvisit/JustvisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justvisit.justvisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustvisitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
