df1ef753774df90006b0c6a45c8c7179e0f79f5e
Rafal <ravmaster48@gmail.com>
2021-05-09 11:13:47 +0100

    draft of index added

---
+++ src/main/java/com/justvisit/justvisit/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.justvisit.justvisit.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
